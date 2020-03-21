8855ab2f47a66ff34bb456ade0626909055af025
Alexandra Collins <andracolexa@gmail.com>
2020-03-21 18:26:09 +0100

    generate elixir/phoenix web app inside the umbrella app

---
+++ config/dev.secret_example.exs
@@ -0,0 +1,8 @@
+# create `dev.secret.exs` file in the root folder and set it up
+# with the data below.
+
+""" use Mix.Config
+
+config :rsvp_web, RsvpWeb.Endpoint,
+  secret_key_base: run `mix phx.gen.secret` to generate a secret_key_base
+"""
